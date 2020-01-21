6ad8275e7d9bdac1ca5b6e0ec1d53ba4863e7095
Cheryl Hughey <cherylhughey@hotmail.com>
2020-01-20 18:48:00 -0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Footer = (props) => (
+    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
+        <p className="copyright">&copy; 2020 Cheryl Hughey <a href="https://cherylhughey.github.io/">Website</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+    </footer>
+)
+
+Footer.propTypes = {
+    timeout: PropTypes.bool
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,67 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="fa fa-cogs"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Your Company Name Here</h1>
+        <p>
+          Your slogan{' '}<br />
+          <a href="http://cherylhughey.com">Learn more</a>
+          <br />
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Intro
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
